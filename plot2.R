7e73bfd434fbb1c1ef5ca6ef3d058dc561cbd891
Steven Wilson <stvn.wlsn@gmail.com>
2015-04-09 12:38:29 +0100

    add plot2 script and png

---
+++ plot2.R
@@ -0,0 +1,12 @@
+dataset <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+
+sub_dataset <- subset(dataset, Date %in% c("1/2/2007", "2/2/2007"))
+
+sub_dataset$DateTime <- as.POSIXct(paste(sub_dataset$Date, sub_dataset$Time), format = "%d/%m/%Y %T")
+
+png(filename = 'plot2.png', width = 480 , height = 480, units = 'px')
+
+plot(sub_dataset$DateTime, as.numeric(as.character(sub_dataset$Global_active_power)), type="l",
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+
+dev.off()
